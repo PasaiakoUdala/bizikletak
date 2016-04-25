14aaa65fbd3dbcf19efc295a218104ae0aaa521f
Iker Ibarguren <ikerib@gmail.com>
2016-04-25 12:36:25 +0200

    js guneak Gmaps

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class DefaultController extends Controller
 {
@@ -13,7 +15,36 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/gunedatuak", name="gunedatuak")
+     */
+    public function gunedatuakAction(Request $request) {
+        $em = $this->getDoctrine()->getManager();
+
+        $guneak = $em->getRepository('AppBundle:Guneak')->findAll();
+
+        $loc = array();
+
+        foreach ($guneak as $g) {
+
+            $l = array (
+                'lat' => $g->getLatitude(),
+                'lon' => $g->getLongitude(),
+                'title' => $g->getIzena(),
+                'html' => "<h3>Ordutegia<//h3><p>" . $g->getOrdutegia() . "<//p>"
+            );
+
+            array_push($loc, $l );
+
+        }
+
+
+        $response = new Response();
+        $response->setContent(json_encode($loc));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
--- src/AppBundle/Entity/Guneak.php
+++ src/AppBundle/Entity/Guneak.php
@@ -58,8 +58,18 @@ class Guneak
     private $kudeatzailea;
 
     /**
-     * @var 
+     * @var float
+     *
+     * @ORM\Column(name="latitude",type="float", nullable=true)
      */
+    private $latitude;
+
+    /**
+     * @var float
+     * 
+     * @ORM\Column(name="longitude",type="float", nullable=true)
+     */
+    private $longitude;
 
 
     /**
@@ -109,6 +119,7 @@ class Guneak
     {
         return $this->getIzena();
     }
+
 
 
     /**
@@ -239,6 +250,54 @@ class Guneak
     public function getKudeatzailea()
     {
         return $this->kudeatzailea;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     *
+     * @return Guneak
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     *
+     * @return Guneak
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
     }
 
     /**
